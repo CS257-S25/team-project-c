e62e48a9fc6ab6728af7f6a24c40aad08995f99a
Mac <mac@MacdeMacBook-Pro-2.local>
2025-05-02 12:33:03 -0500

    create table

---
+++ Data/createtable.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS ufo;
+CREATE TABLE ufo (
+  ufo_date text,
+  ufo_city text,
+  ufo_state text,
+  ufo_country text,
+  ufo_shape text,
+  ufo_duration int,
+  ufo_comment text
+);
